c3b8d1e4e4bb656ae1adc4347c37896d7ad5d6da
Angelo Moreira <angelo.moreira@gmail.com>
2019-05-02 22:54:12 +0100

    our mini test framework completed

--- module_extension_custom.exs
+++ module_extension_custom.exs
@@ -1,12 +1,55 @@
 defmodule Assertion do
   defmacro __using__(_options) do
     quote do
-      # import unquote(__MODULE__)
+      import unquote(__MODULE__)
+      Module.register_attribute(__MODULE__, :tests, accumulate: true)
 
       def run do
-        IO.puts("Running the tests...")
+        IO.puts("Running the tests (#{inspect(@tests)})")
       end
     end
+  end
+
+  # {:==, [context: Elixir, import:  Kernel], [5,5]}
+  defmacro assert({operator, _, [lhs, rhs]}) do
+    quote bind_quoted: [operator: operator, lhs: lhs, rhs: rhs] do
+      Assertion.Test.assert(operator, lhs, rhs)
+    end
+  end
+
+  defmacro test(description, do: test_block) do
+    test_func = String.to_atom(description)
+
+    quote do
+      @tests {unquote(test_func), unquote(description)}
+      def unquote(test_func)(), do: unquote(test_block)
+    end
+  end
+end
+
+defmodule Assertion.Test do
+  def assert(:==, lhs, rhs) when lhs == rhs do
+    IO.write(".")
+  end
+
+  def assert(:==, lhs, rhs) do
+    IO.puts("""
+    FAILURE:
+    Expected:         #{lhs}
+    to be equal to:   #{rhs}
+    """)
+  end
+
+  def assert(:>, lhs, rhs) when lhs > rhs do
+    IO.write(".")
+  end
+
+  def assert(:>, lhs, rhs) do
+    IO.puts("""
+    FAILURE:
+    Expected:             #{lhs}
+    to be greater than:   #{rhs}
+    """)
   end
 end
 
