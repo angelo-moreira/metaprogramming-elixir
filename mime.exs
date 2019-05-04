a514ddf173b22d36b8c533c7fc9b1ec6911ae5ba
Angelo Moreira <angelo.moreira@gmail.com>
2019-05-04 22:52:38 +0100

    add mime program to list valid mime types

---
+++ mime.exs
@@ -0,0 +1,15 @@
+defmodule Mime do
+  extensions_file = Path.join([__DIR__, "mimes.txt"])
+
+  for line <- File.stream!(extensions_file, [], :line) do
+    [type, rest] = line |> String.split("\t") |> Enum.map(&String.strip/1)
+    extensions = String.split(rest, ~r/,\s?/)
+
+    def exts_from_type(unquote(type)), do: unquote(extensions)
+    def type_from_ext(ext) when ext in unquote(extensions), do: unquote(type)
+  end
+
+  def exts_from_type(_type), do: []
+  def type_from_ext(_ext), do: nil
+  def valid_type?(type), do: type |> exts_from_type() |> Enum.any?()
+end
