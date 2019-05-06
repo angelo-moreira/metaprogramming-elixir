883bee317f7800c0becabd67ab17298032340900
Angelo Moreira <angelo.moreira@gmail.com>
2019-05-06 23:16:40 +0100

    first version of the translation library

--- translator.exs
+++ translator.exs
@@ -1,7 +1,7 @@
 defmodule Translator do
   defmacro __using__(_options) do
     quote do
-      Module.register_attribute(__MODULE__, :locales, accumulate: true, persiste: false)
+      Module.register_attribute(__MODULE__, :locales, accumulate: true, persist: false)
 
       import unquote(__MODULE__), only: [locale: 2]
       @before_compile unquote(__MODULE__)
@@ -24,14 +24,38 @@ defmodule Translator do
         deftranslations(locale, "", mappings)
       end
 
-    quote do
-      def t(locale, path, bindings \\ [])
-      unquote(translations_ast)
-      def t(_locale, _path, _bindings), do: {:error, :no_translation}
+    final_ast =
+      quote do
+        def t(locale, path, bindings \\ [])
+        unquote(translations_ast)
+        def t(_locale, _path, _bindings), do: {:error, :no_translation}
+      end
+
+    IO.puts(Macro.to_string(final_ast))
+    final_ast
+  end
+
+  defp deftranslations(locale, current_path, mappings) do
+    for {key, val} <- mappings do
+      path = append_path(current_path, key)
+
+      if Keyword.keyword?(val) do
+        deftranslations(locale, path, val)
+      else
+        quote do
+          def t(unquote(locale), unquote(path), bindings) do
+            unquote(interpolate(val))
+          end
+        end
+      end
     end
   end
 
-  defp deftranslations(locales, current_path, mappings) do
-    # TBD: returns an AST of the t/3 functions defs for the given locale
+  defp interpolate(string) do
+    # TBD interpolate bindings within string
+    string
   end
+
+  defp append_path("", next), do: to_string(next)
+  defp append_path(current, next), do: "#{current}.#{next}"
 end
